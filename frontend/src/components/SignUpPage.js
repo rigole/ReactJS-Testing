fd0a41f9708e9a5ccd3d67c192d82f87185508e1
rigole <foplacide@gmail.com>
2022-08-15 11:15:34 +0100

    disable on button on state

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -1,13 +1,55 @@
-import React  from "react";
+import React,{useState}  from "react";
 
 function SignUpPage () {
+    const [disabled, setDisabled] = useState(true)
+    const [password, setPassword] = useState('')
+    const [passwordRepeat, setPasswordRepeat] = useState('')
+
+
+    const onChangePassword = (event) =>{
+         const currentValue = event.target.value
+         setPassword(currentValue)
+        if (currentValue == passwordRepeat){
+            setDisabled(false)
+        }
+        else{
+            setDisabled(true)
+        }
+
+        console.log(disabled)
+    }
+
+
+
+    const onChangePasswordRepeat = (event) =>{
+        const currentValue = event.target.value;
+       setPasswordRepeat(currentValue)
+
+        if (currentValue == password){
+            setDisabled(false)
+        } else{
+            setDisabled(true)
+        }
+        console.log(disabled)
+    }
+
+    /*setTimeout(() =>{
+        console.log("Updating disabled")
+        setDisabled(false)
+    }, 1000);*/
         return (
+
             <div>
-                <h1>Hello world</h1>
-                <label>Username</label>
+                <h1>Sign Up</h1>
+                <label htmlFor="username">username</label>
                     <input type="text" placeholder="username"/>
                 <label>Email</label>
-                    <input type="text" placeholder="email"/>
+                    <input type="email" placeholder="email"/>
+                <label htmlFor="password">password</label>
+                    <input id="password" type="password" onChange={onChangePassword}/>
+                <label htmlFor="passwordRepeat">Password Repeat</label>
+                    <input id="passwordRepeat" type="password" onChange={onChangePasswordRepeat}/>
+                <button disabled={disabled}>Sign up</button>
             </div>
         )
 }
--- frontend/src/components/SignUpPage.test.js
+++ frontend/src/components/SignUpPage.test.js
@@ -1,6 +1,7 @@
 import SignUpPage from "./SignUpPage";
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
 const sign = require("./SignUpPage")
 
 
@@ -20,17 +21,28 @@ describe('Sign Up Page', () => {
             const { container } = render(<SignUpPage/>);
             const inputs = container.querySelectorAll('input');
             expect(inputs.length).toBe(2);
-        });*/
-        it('has username input ', () => {
+        });
+        it('has username label', () => {
             render(<SignUpPage/>);
-            const input = screen.getByPlaceholderText('username');
+            const input = screen.getByLabelText('username');
             expect(input).toBeInTheDocument();
         });
         it('has email input', () => {
             render(<SignUpPage/>);
             const input = screen.getByPlaceholderText('email');
             expect(input).toBeInTheDocument();
-        });
-    })
+        });*/
+    });
+     describe("Testing interactions" , () => {
+         it("enables the button when password have same savuel" , ()=> {
+             render(<SignUpPage/>);
+             const passwordInput = screen.getByLabelText('password');
+             const passwordRepeat = screen.getByLabelText("Password Repeat")
+             userEvent.type(passwordInput, "Password");
+             userEvent.type(passwordRepeat, "Password");
+             const button = screen.queryByRole('button', { name: 'Sign Up' });
+             expect(button).toBeDisabled();
+         })
+     })
 })
 
